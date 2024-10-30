57a3f6ba8057bf2dddb5582717847d5b13fd4ca6
pb <pb@gauss-lvs.de>
2024-10-30 09:08:52 +0100

    NuGet-Paket wird auch auf den NuGet-Server hochgeladen

--- publish.ps1
+++ publish.ps1
@@ -1,4 +1,7 @@
 dotnet version -f .\src\dotnet-version.csproj build `
   && dotnet build .\src\dotnet-version.csproj -c Release `
-  && dotnet pack .\src\dotnet-version.csproj -o P:\Entwicklung\NugetPackages -c Release `
+  && dotnet pack .\src\dotnet-version.csproj -o ".nupkgs" -c Release `
+  && Copy-Item ".nupkgs\*.nupkg" $env:GAUSS_NUGET_FOLDER `
+  && dotnet nuget push ".nupkgs\*.nupkg" --source gauss --api-key $env:GAUSS_NUGET_API_KEY --skip-duplicate `
+  && Remove-Item ".nupkgs" -Recurse `
   && git push
